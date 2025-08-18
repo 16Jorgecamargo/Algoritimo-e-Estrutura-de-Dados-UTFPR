521684f84e6f16533e7845a8a18a0888634030af
Jorge Daniel Ristow de Camargo <92612384+16Jorgecamargo@users.noreply.github.com>
2025-08-18 20:20:08 -0300

    feat: resolver questão 2 de ponteiros
    Implementar solução para questão 2 da segunda lista.

--- src/listas/ponteiros/core/2.c
+++ src/listas/ponteiros/core/2.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "../headlers/2.h"
 #include "../../../shared/headlers/color.h"
-
-// Questao 2: [Descricao sera preenchida conforme PDF]
+#include "../../../shared/headlers/colorPrint.h"
+#include "../../../shared/headlers/clean.h"
 
 void executarQuestaoPonteiros2(void) {
-    printf("=== Questao 2 - Ponteiros ===\n");
-    printf("[Titulo da questao]\n\n");
-    
-    // TODO: Implementar a solução
-    setColor(RED);
-    printf("Esta questao ainda nao foi implementada.\n");
+
+    limparTela();
+    setColor(YELLOW);
+    printf("=== Ponteiros ===\n");
+    printf("=== Executando a Questao 02 ===\n\n");
     resetColor();
-    printf("Implemente a funcao neste arquivo.\n");
+
+printf("main()\n");
+printf("{\n");
+printf("  int y, *p, x;\n");
+printf("  y = 0; ");
+printMensagemColorida(4, "// o y recebeu o valor 0");
+printf("  p = &y; ");
+printMensagemColorida(4, "// o p recebeu o endereco de y");
+printf("  x = *p; ");
+printMensagemColorida(4, "// o x recebeu o conteudo de *p, entao x=0");
+printf("  x = 4; ");
+printMensagemColorida(4, "// o x recebeu o valor 4");
+printf("  (*p)++;\n");
+printf("  x--;\n");
+printf("  (*p) += x;");
+printMensagemColorida(4, "// o conteudo de p recebeu o conteudo de p + x, ou seja, y = 0 + 3");
+printf("  printf (\"y = %%d\\n\", y);");
+printMensagemColorida(4, "// vai imprimir y = 3, pois y foi alterado indiretamente com o ponteiro p");
+printf("  return(0);\n");
+printf("}\n");
 }

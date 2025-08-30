0cfcd8cbed2f8c629fa95d1349e5b3e2809c699f
Jorge Daniel Ristow de Camargo <92612384+16Jorgecamargo@users.noreply.github.com>
2025-08-30 06:42:56 -0300

    feat: resolver questão 4 de pilhas dinâmicas
    Implementar solução para questão 4 da terceira lista.

--- src/listas/pilhaDinamica/core/4.c
+++ src/listas/pilhaDinamica/core/4.c
@@ -1,17 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../headlers/4.h"
+#include <time.h>
+#include "../../../shared/headlers/pilhaDinamica.h"
 #include "../../../shared/headlers/color.h"
+#include "../../../shared/headlers/colorPrint.h"
+#include "../../../shared/headlers/clean.h"
+#include "../headlers/4.h"
 
-// Questao 4: [Descricao sera preenchida conforme PDF]
+PilhaQuestao4 *cria_PilhaQuestao4()
+{
+    PilhaQuestao4 *pi = (PilhaQuestao4 *)malloc(sizeof(PilhaQuestao4));
+    if (pi != NULL) *pi = NULL;
+    return pi;
+}
+
+void libera_PilhaQuestao4(PilhaQuestao4 *pi)
+{
+    if (pi != NULL)
+    {
+        ElemQuestao4 *no;
+        while ((*pi) != NULL) { no = *pi; *pi = (*pi)->prox; free(no); }
+        free(pi);
+    }
+}
+
+int insere_PilhaQuestao4(PilhaQuestao4 *pi, int n)
+{
+    if (pi == NULL) return 0;
+    ElemQuestao4 *no;
+    no = (ElemQuestao4 *)malloc(sizeof(ElemQuestao4));
+    if (no == NULL) return 0;
+    no->n = n;
+    no->prox = (*pi);
+    *pi = no;
+    return 1;
+}
+
+int tamanho_PilhaQuestao4(PilhaQuestao4 *pi)
+{
+    if (pi == NULL) return 0;
+    int cont = 0;
+    ElemQuestao4 *no = *pi;
+    while (no != NULL) { cont++; no = no->prox;}
+    return cont;
+}
+
+void imprime_PilhaQuestao4(PilhaQuestao4 *pi)
+{
+    if (pi == NULL) return;
+    ElemQuestao4 *no = *pi;
+    printf("-------------------------------\n");
+    while (no != NULL)
+    {
+        printf("Numero: %d\n", no->n);
+        printf("-------------------------------\n");
+        no = no->prox;
+    }
+}
+
+void verificaMMM_PilhaQuestao4(PilhaQuestao4 *pi)
+{
+    int maior= 0, menor= 0, media= 0;
+    if (pi == NULL) return;
+    ElemQuestao4 *no = *pi;
+    while (no != NULL)
+    {
+        if (no->n > maior) maior = no->n;
+        if (no->n < menor || menor == 0) menor = no->n;
+        media += no->n;
+        no = no->prox;
+    }
+    media /= tamanho_PilhaQuestao4(pi);
+    printMensagemColoridaFormatted(6,"Maior: %d", maior);
+    printMensagemColoridaFormatted(6,"Menor: %d", menor);
+    printMensagemColoridaFormatted(6,"Media: %d", media);
+}
 
 void executarQuestaoPilhaDinamica4(void) {
-    printf("=== Questao 4 - Pilha Dinamica ===\n");
-    printf("[Titulo da questao]\n\n");
-    
-    // TODO: Implementar a solução
-    setColor(RED);
-    printf("Esta questao ainda nao foi implementada.\n");
-    resetColor();
-    printf("Implemente a funcao neste arquivo.\n");
+    srand((unsigned)time(NULL));
+    PilhaQuestao4* pi = cria_PilhaQuestao4();
+    printMensagemColorida(6,"Pilha criada com sucesso!");
+    printMensagemColoridaFormatted(5,"Tamanho atual: %d\n",tamanho_PilhaQuestao4(pi));
+    int n = (rand() % 15) + 1;
+    printMensagemColorida(6,"Adicionando quantidade aleatoria de elementos...");
+    for (int i = 0; i < n; i++) { n = (rand() % 100) + 1; insere_PilhaQuestao4(pi, n); }
+    imprime_PilhaQuestao4(pi);
+    printMensagemColoridaFormatted(5,"Tamanho atual: %d\n",tamanho_PilhaQuestao4(pi));
+    verificaMMM_PilhaQuestao4(pi);
+    libera_PilhaQuestao4(pi);
 }
--- src/listas/pilhaDinamica/headlers/4.h
+++ src/listas/pilhaDinamica/headlers/4.h
@@ -1,7 +1,21 @@
 #ifndef PILHADINAMICA_4_H
 #define PILHADINAMICA_4_H
 
-// Questao 4: [Descricao sera preenchida conforme PDF]
+struct elementoquestao4
+{
+    int n;
+    struct elementoquestao4 *prox;
+};
+
+typedef struct elementoquestao4 ElemQuestao4;
+typedef struct elementoquestao4* PilhaQuestao4;
+
+PilhaQuestao4 *cria_PilhaQuestao4();
+void libera_PilhaQuestao4(PilhaQuestao4 *pi);
+int insere_PilhaQuestao4(PilhaQuestao4 *pi, int n);
+int tamanho_PilhaQuestao4(PilhaQuestao4 *pi);
+void imprime_PilhaQuestao4(PilhaQuestao4 *pi);
+void verificaMMM_PilhaQuestao4(PilhaQuestao4 *pi);
 void executarQuestaoPilhaDinamica4(void);
 
 #endif
